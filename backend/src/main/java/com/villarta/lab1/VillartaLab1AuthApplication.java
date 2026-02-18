8e7cf5c18fc76029eaced91c54ac8b17842f3417
hetorydetorydak <johnhextor312@gmail.com>
2026-02-18 20:48:27 +0800

    setup(backend): initialize backend

---
+++ backend/src/main/java/com/villarta/lab1/VillartaLab1AuthApplication.java
@@ -0,0 +1,13 @@
+package com.villarta.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillartaLab1AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillartaLab1AuthApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/villarta/lab1/VillartaLab1AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.villarta.lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillartaLab1AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

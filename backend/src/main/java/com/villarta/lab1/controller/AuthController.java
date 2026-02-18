b5840379d582b5145c256db7ec343f066b43c7f9
hetorydetorydak <johnhextor312@gmail.com>
2026-02-18 23:33:33 +0800

    feature(backend): added Auth and User Controllers

---
+++ backend/src/main/java/com/villarta/lab1/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.villarta.lab1.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.villarta.lab1.dto.ApiResponse;
+import com.villarta.lab1.dto.LoginRequest;
+import com.villarta.lab1.dto.LoginResponse;
+import com.villarta.lab1.dto.RegisterRequest;
+import com.villarta.lab1.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ApiResponse register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
